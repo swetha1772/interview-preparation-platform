497c0ae5d947c6f9edebe276110672159f12d42d
swetha1772 <swethapalepu47@gmail.com>
2026-06-26 22:05:24 +0530

    Use environment variable for database connection

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:swetha@localhost/interview_platform"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:swetha@localhost/interview_platform"
+)
 
 engine = create_engine(DATABASE_URL)
 
